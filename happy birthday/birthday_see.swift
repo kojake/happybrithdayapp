6509c6818ec194e82ae0e42bf64e1974ec0ed4e0
kaito <takahashikaito@kaitonoMacBook-Air.local>
2023-02-12 09:21:33 +0900

    update reload_button add

--- happy birthday/birthday_see.swift
+++ happy birthday/birthday_see.swift
@@ -11,25 +11,32 @@ class birthday_see: UIViewController, UITableViewDataSource,UITableViewDelegate{
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        birthday_list = UserDefaults.standard.object(forKey: "birthday_list_key") as! [String]
     }
-    
-    var birthday_list_see = UserDefaults.standard.object(forKey: "birthday_list_key") as! [String]
     
     @IBOutlet weak var tableview: UITableView!
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return birthday_list_see.count
+        return birthday_list.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-        cell.textLabel?.text = birthday_list_see[indexPath.row]
+        cell.textLabel?.text = birthday_list[indexPath.row]
         return cell
     }
+    
+    //読み込む
+    @IBAction func reload(_ sender: Any) {
+        viewDidLoad()
+        tableview.reloadData()
+    }
+    
     //セルの消去
     func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        birthday_list_see.remove(at: indexPath.row)
+        birthday_list.remove(at: indexPath.row)
         tableview.deleteRows(at: [indexPath], with: .top)
-        UserDefaults.standard.set(birthday_list_see, forKey: "birthday_list_key")
+        UserDefaults.standard.set(birthday_list, forKey: "birthday_list_key")
     }
 }
