83490c3128dd7bfceef2eef0ba9e2ac3179f6e09
Yuki Takatsu <46854753+yuuki00682200@users.noreply.github.com>
2023-02-25 11:00:04 +0900

    Update birthday_see.swift

--- happy birthday/birthday_see.swift
+++ happy birthday/birthday_see.swift
@@ -1,45 +1,22 @@
-//
-//  birthday_see.swift
-//  happy birthday
-//
-//  Created by kaito on 2022/12/11.
-//
-
 import UIKit
 
-class birthday_see: UIViewController, UITableViewDataSource,UITableViewDelegate{
-    
-    //tableview
-    @IBOutlet weak var tableview: UITableView!
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        birthday_list = UserDefaults.standard.object(forKey: "birthday_list_key") as! [String]
-        
-//        //tableviewcustom
-//        tableview.register(UINib(nibName: "customCell", bundle: nil), forCellReuseIdentifier: "customCell")
-    }
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return birthday_list.count
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        if tableView.dequeueReusableCell(withIdentifier: "customCell") is customCell {
-            let cell = tableView.dequeueReusableCell(withIdentifier: "customCell", for: indexPath)
-            cell.textLabel?.text = birthday_list[indexPath.row]
-            
-            return cell
+class customCell: UITableViewCell {    
+    @IBOutlet weak var photoimageview: UIImageView!
+
+    //photoimageviewの画像をbirthday_see.swiftに渡す
+    var photoimage: UIImage? {
+        didSet {
+            photoimageview.image = photoimage
         }
-        return UITableViewCell()
-    }
-    
-    //セルの消去
-    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        birthday_list.remove(at: indexPath.row)
-        tableview.deleteRows(at: [indexPath], with: .top)
-        UserDefaults.standard.set(birthday_list, forKey: "birthday_list_key")
     }
 
+     
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+    
 }
